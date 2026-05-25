074a3420c0ffe9c294e6c348a69f52d08d18d37e
Sainath <sainathkondri2002@gmail.com>
2026-05-25 02:19:48 -0400

    Add FastAPI prediction API and request schema

---
+++ api/main.py
@@ -0,0 +1,59 @@
+import pickle
+import pandas as pd
+
+from fastapi import FastAPI
+from api.schema import CustomerData
+
+
+app = FastAPI(
+    title="Customer Churn Prediction API",
+    description="API for predicting customer churn using a trained machine learning model.",
+    version="1.0.0"
+)
+
+
+MODEL_PATH = "models/churn_model.pkl"
+
+
+with open(MODEL_PATH, "rb") as file:
+    model_artifact = pickle.load(file)
+
+
+model = model_artifact["model"]
+scaler = model_artifact["scaler"]
+feature_names = model_artifact["feature_names"]
+model_name = model_artifact["model_name"]
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "Customer Churn Prediction API is running",
+        "model": model_name
+    }
+
+
+@app.post("/predict")
+def predict_churn(customer: CustomerData):
+
+    input_data = pd.DataFrame([customer.model_dump()])
+
+    input_encoded = pd.get_dummies(input_data, drop_first=True)
+
+    input_encoded = input_encoded.reindex(
+        columns=feature_names,
+        fill_value=0
+    )
+
+    input_scaled = scaler.transform(input_encoded)
+
+    prediction = model.predict(input_scaled)[0]
+    probability = model.predict_proba(input_scaled)[0][1]
+
+    result = "Churn" if prediction == 1 else "No Churn"
+
+    return {
+        "prediction": result,
+        "churn_probability": round(float(probability), 4),
+        "model_used": model_name
+    }
